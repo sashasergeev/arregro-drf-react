08f8f1724d00270921d174aca6bf2fd1a24e07ac
sashasergeev <2ndmission3@gmail.com>
2021-11-13 23:19:08 +0300

    Resolved websocket issue on deploy.

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web: gunicorn arregro.wsgi --log-file -
+web: daphne arregro.asgi:application --port $PORT --bind 0.0.0.0 -v2
 celery: celery -A arregro worker --concurrency 1 -P solo
 celerybeat: celery -A arregro beat -l INFO
 celeryworker: celery -A arregro beat & celery -A arregro worker --concurrency 1 solo -l INFO & wait -n
--- frontend/src/components/posts/Posts.js
+++ frontend/src/components/posts/Posts.js
@@ -26,16 +26,15 @@ export class Posts extends Component {
       newPost: 0,
     };
   }
-  newPostWS = new WebSocket("ws://" + window.location.host + "/ws/new-post/");
+  newPostWS = new WebSocket(
+    (window.location.protocol === "http:" ? "ws://" : "wss://") +
+      window.location.host +
+      "/ws/new-post/"
+  );
   componentDidMount() {
     // sockets
-    // const socket = new WebSocket(
-    //   "ws://" + window.location.host + "/ws/new-post/"
-    // );
     this.newPostWS.onmessage = (e) => {
-      console.log("hello");
       this.setState({ newPost: this.state.newPost + 1 });
-      console.log(e);
     };
     // data
     this.getCardsData();
